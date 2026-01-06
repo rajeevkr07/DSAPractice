5b6912c7785a3bd40c0f8575ebb299ef32e78b3a
Rajeev Kumar Raja <75115330+rajeevkr07@users.noreply.github.com>
2026-01-06 23:24:39 +0530

    Time: 0 ms (100%), Space: 11.6 MB (77.22%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+
+        int i =0; //dedicate to 0
+        int j= 0; // dedicated to 1
+        int k = n-1; // dedicated to 2
+
+        while( j <= k){
+            if(nums[j]==1)
+            {
+                j++;
+            } else if(nums[j]==2)
+            {
+                swap(nums[j],nums[k]);
+                k--;
+             }else
+              { // nums[j]==0
+                 swap(nums[j],nums[i]);
+                 i++;
+                 j++;
+               }
+        }
+    }
+};
