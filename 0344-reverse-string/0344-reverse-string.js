d9832a81a3c3955bfe4a77a6308294da78215cb7
Rajeev Kumar Raja <75115330+rajeevkr07@users.noreply.github.com>
2026-01-08 00:19:25 +0530

    Time: 0 ms (100%), Space: 60.9 MB (63.19%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,5 +3,13 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    console.log(... s.reverse());
+    // console.log(... s.reverse());
+    let left = 0, right = s.length-1
+    while(left < right){
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        right --;
+        left ++;
+    }
 };
